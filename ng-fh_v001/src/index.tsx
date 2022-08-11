8246c2e185c12172c9e1ea4523f64fce47099fc4
niuge <694838286@qq.com>
2022-08-12 00:28:28 +0800

    搭建基本目录

--- ng-fh_v001/src/index.tsx
+++ ng-fh_v001/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.less';
 import 'antd/dist/antd.min.css';
 // import 'antd/dist/antd.css';
 import App from './App';
